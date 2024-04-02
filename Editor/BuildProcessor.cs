2177395a3fab506ef4fa8cc160ac71d61f83d2d9
Krystian Babilinski <kbabilinski@gmail.com>
2024-04-02 15:36:18 -0400

    Update BuildProcessor to support Android x86_64

--- Editor/BuildProcessor.cs
+++ Editor/BuildProcessor.cs
@@ -36,7 +36,7 @@ namespace Unity.WebRTC.Editor
         /// <summary>
         ///
         /// </summary>
-        public const AndroidArchitecture RequiredAndroidArchitectures = AndroidArchitecture.ARM64;
+        public const AndroidArchitecture RequiredAndroidArchitectures = AndroidArchitecture.ARM64 | AndroidArchitecture.X86_64;
 
         /// <summary>
         ///
@@ -86,10 +86,10 @@ namespace Unity.WebRTC.Editor
         /// </summary>
         static void EnsureAndroidArchitecture()
         {
-            if (PlayerSettings.Android.targetArchitectures != AndroidArchitecture.ARM64)
+            if ((PlayerSettings.Android.targetArchitectures & RequiredAndroidArchitectures) == 0)
             {
                 Debug.LogWarning(
-                    $"WebRTC apps require a target architecture to be set {RequiredAndroidArchitectures}. " +
+                    $"WebRTC apps require a target architecture to include {RequiredAndroidArchitectures}. " +
                     $"Currently set to {PlayerSettings.Android.targetArchitectures}");
             }
         }
